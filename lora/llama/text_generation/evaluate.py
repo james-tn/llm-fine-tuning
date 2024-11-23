a72b26ae68ca0e8182e3278747437ca16354181e
james-tn <james.nguyen@microsoft.com>
2024-11-23 13:57:31 -0800

    change lora & llama with deepspeed

---
+++ lora/llama/text_generation/evaluate.py
@@ -0,0 +1,238 @@
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    BitsAndBytesConfig,
+    HfArgumentParser,
+    TrainingArguments,
+    GenerationConfig,
+    pipeline,
+    logging,
+    
+)
+from peft import LoraConfig, PeftModel  
+
+import torch
+import mlflow
+import argparse
+import os
+import pandas as pd
+import json  
+import re  
+import random
+class LAMA2Predict(mlflow.pyfunc.PythonModel):
+    pass
+
+
+def load_model(model, tokenizer):
+    """
+    This function is called when the container is initialized/started, typically after create/update of the deployment.
+    You can write the logic here to perform init operations like caching the model in memory
+    """
+    
+
+  
+    # device_map = "auto"
+    # model_path = os.path.join(
+    #     base_path, "data/model"
+    # )
+    # tokenizer_path =  os.path.join(
+    #     base_path, "data/tokenizer"
+    # )
+
+
+    # model = AutoModelForCausalLM.from_pretrained(
+    #     model_path,
+    #     local_files_only=True,
+    #     low_cpu_mem_usage=True,
+    #     return_dict=True,
+    #     torch_dtype=torch.float16,
+    #     device_map=device_map,
+    # )
+    # tokenizer = AutoTokenizer.from_pretrained(
+    #     tokenizer_path,
+    #     local_files_only=True,
+    #     device_map=device_map
+    # )
+    model.generation_config.pad_token_id = tokenizer.pad_token_id
+
+    scoring_pipeline = pipeline(task="text-generation", model=model, tokenizer=tokenizer)
+
+    return scoring_pipeline, tokenizer
+
+
+def run(scoring_pipeline, tokenizer, input_text):
+    """
+    This function is called for every invocation of the endpoint to perform the actual scoring/prediction.
+    In the example we extract the data from the json input and call the pipeline
+    method and return the result back
+    """
+    gen_config = GenerationConfig(max_new_tokens=180, temperature= 0.1, stop_strings=["### End"] )
+    result = scoring_pipeline(input_text, generation_config=gen_config,tokenizer=tokenizer)
+    return result
+
+
+def parse_args():
+    # setup arg parser
+    parser = argparse.ArgumentParser()
+
+
+    # add arguments
+    parser.add_argument("--chat_model", type=str, default="False")
+    parser.add_argument("--epochs", type=int, default=1)
+    parser.add_argument("--num_examples", type=int, default=1000)
+    parser.add_argument("--model_name", type=str)
+    parser.add_argument("--trained_model", type=str, default="trained_model")
+    parser.add_argument("--model_dir", type=str)
+    parser.add_argument("--evaluated_model", type=str)
+    parser.add_argument("--test_dataset", type=str)
+
+    # parse args
+    args = parser.parse_args()
+
+    # return args
+    return args
+  
+def process_jsonl(file_path):  
+    with open(file_path, 'r') as file:  
+        return [json.loads(line) for line in file]  
+  
+def extract_input(record):  
+    output_index = record.index('### Output:')  
+    return record[:output_index + len('### Output:')]  
+  
+def extract_output(record):  
+    output_index = record.index('### Output:') + len('### Output:')  
+  
+    try:  
+        # Try to find the end index using "### End"  
+        end_index = record.index('### End', output_index)  
+    except ValueError:  
+        # If "### End" is not found, set end_index to 16 characters after "### Output:"  
+        end_index = output_index + 35  
+  
+    return record[output_index:end_index]  
+  
+def extract_codes(output_text):  
+    # Update the regex to match numbers with optional dots  
+    match = re.search(r'\b((?:\d+\.)*\d+)\b', output_text)  
+    if match:  
+        # Return the matched number as a string without dots  
+        return match.group(1).replace('.', '')    
+def run_scoring_pipeline(scoring_pipeline, tokenizer, batch):  
+    return run(scoring_pipeline, tokenizer, batch)
+def extract_codes1(output_text):  
+    # Split the text into lines  
+    lines = output_text.split('\n')  
+      
+    # Initialize variables to store the extracted numbers  
+    chapter, heading, subheading = None, None, None  
+      
+    # Iterate over each line to find the relevant codes  
+    for line in lines:  
+        if 'Chapter:' in line:  
+            # Extract the chapter number, removing any non-numeric characters  
+            chapter_match = re.search(r'Chapter:\s*([\d.]+)', line)  
+            if chapter_match:  
+                chapter = re.sub(r'\D', '', chapter_match.group(1))  
+          
+        elif 'Heading:' in line:  
+            # Extract the heading number, removing any non-numeric characters  
+            heading_match = re.search(r'Heading:\s*([\d.]+)', line)  
+            if heading_match:  
+                heading = re.sub(r'\D', '', heading_match.group(1))  
+          
+        elif 'Subheading:' in line:  
+            # Extract the subheading number, removing any non-numeric characters  
+            subheading_match = re.search(r'Subheading:\s*([\d.]+)', line)  
+            if subheading_match:  
+                subheading = re.sub(r'\D', '', subheading_match.group(1))  
+      
+    return chapter, heading, subheading  
+  
+def calculate_accuracy(true_values, predicted_values):  
+    correct_chapter = sum(1 for true, pred in zip(true_values, predicted_values) if true[:2] == pred[:2])  
+    correct_heading = sum(1 for true, pred in zip(true_values, predicted_values) if true[:4] == pred[:4])  
+    correct_subheading = sum(1 for true, pred in zip(true_values, predicted_values) if true == pred)  
+    total = len(true_values)  
+  
+    return {  
+        'chapter_accuracy': correct_chapter / total,  
+        'heading_accuracy': correct_heading / total,  
+        'subheading_accuracy': correct_subheading / total  
+    }  
+  
+def evaluate(scoring_pipeline, tokenizer, file_path, batch_size=5):  
+    records = process_jsonl(file_path)  
+    # Randomly sample 1000 records  
+    sampled_records = random.sample(records, min(100, len(records)))  
+
+    inputs = [extract_input(record['record']) for record in sampled_records]  
+    true_outputs = [extract_codes(extract_output(record['record'])) for record in sampled_records]
+      
+    predicted_outputs = []  
+    for i in range(0, len(inputs), batch_size):  
+        batch = inputs[i:i + batch_size]  
+        batch_results = run_scoring_pipeline(scoring_pipeline, tokenizer,batch)  
+        for result in batch_results:  
+            text_result = result[0]['generated_text']
+            predicted_outputs.append(extract_codes(extract_output(text_result)))  
+    print("predicted_outputs", predicted_outputs)
+    accuracy = calculate_accuracy(true_outputs, predicted_outputs) 
+    # Log the accuracy metrics with MLflow  
+    with mlflow.start_run():  
+        mlflow.log_metric("chapter_accuracy", accuracy['chapter_accuracy'])  
+        mlflow.log_metric("heading_accuracy", accuracy['heading_accuracy'])  
+        mlflow.log_metric("subheading_accuracy", accuracy['subheading_accuracy'])  
+      
+    print("Accuracies:", accuracy)  
+ 
+  
+
+def main(args):
+    trained_model = args.trained_model
+    model_name = args.model_name
+    test_dataset = args.test_dataset
+    model_dir = args.model_dir  
+    evaluated_model = args.evaluated_model
+
+    chat_model = args.chat_model
+    model_artifact_path = "mlflow_model_folder"
+
+    print("content of model path", os.listdir(trained_model))
+    
+    base_model = AutoModelForCausalLM.from_pretrained(  
+        os.path.join(model_dir, "data", "model") ,  
+        local_files_only=True,
+        torch_dtype=torch.float16,
+
+    )  
+    model = PeftModel.from_pretrained(base_model, os.path.join(trained_model, "lora"))
+    model = model.merge_and_unload()
+    tokenizer = AutoTokenizer.from_pretrained(  
+         os.path.join(trained_model, "lora"),  
+        local_files_only=True,  
+        device_map="auto"  
+    )  
+
+
+    scoring_pipeline, tokenizer = load_model(model, tokenizer)
+    print("pipeline loaded")
+    evaluate(scoring_pipeline, tokenizer, test_dataset) 
+    print("model evaluated")
+    model.save_pretrained(evaluated_model)  
+    # # Save the tokenizer  
+    tokenizer.save_pretrained(evaluated_model)  
+
+    with mlflow.start_run() as run:
+            os.environ["AZUREML_ARTIFACTS_DEFAULT_TIMEOUT"] = "1800" #give time for model to be registered
+            mlflow.pyfunc.log_model(artifacts={"model": evaluated_model}, artifact_path=model_artifact_path, python_model=LAMA2Predict())
+            model_uri = f"runs:/{run.info.run_id}/{model_artifact_path}"
+            mlflow.register_model(model_uri, name = model_name,await_registration_for=1800)
+
+
+if __name__ == "__main__":
+    # parse args
+    args = parse_args()
+
+    # run main function
+    main(args)
---
+++ lora/llama/text_generation/fine_tune.py
@@ -0,0 +1,300 @@
+import os  
+import torch  
+import math  
+import argparse  
+import mlflow  
+import pandas as pd  
+from datasets import Dataset  
+from transformers import (  
+    AutoModelForCausalLM,  
+    AutoTokenizer,  
+    BitsAndBytesConfig,  
+    TrainingArguments,  
+    TrainerCallback  
+)  
+import shutil  
+
+from deepspeed.utils.zero_to_fp32 import get_fp32_state_dict_from_zero_checkpoint, load_state_dict_from_zero_checkpoint  
+import subprocess
+from transformers.integrations import MLflowCallback  
+from peft import LoraConfig, PeftModel  
+from trl import SFTTrainer, DataCollatorForCompletionOnlyLM  
+  
+def str2bool(v):  
+    if isinstance(v, bool):  
+        return v  
+    if v.lower() in ('yes', 'true', 't', 'y', '1'):  
+        return True  
+    elif v.lower() in ('no', 'false', 'f', 'n', '0'):  
+        return False  
+    else:  
+        raise argparse.ArgumentTypeError('Boolean value expected.')  
+  
+def get_latest_checkpoint_tag(checkpoint_dir):  
+    """  
+    Retrieves the latest checkpoint tag by sorting the directory names.  
+  
+    :param checkpoint_dir: The path to the directory containing checkpoint subdirectories.  
+    :return: The tag of the latest checkpoint.  
+    """  
+    # List all subdirectories in the checkpoint directory  
+    subdirs = [d for d in os.listdir(checkpoint_dir) if os.path.isdir(os.path.join(checkpoint_dir, d))]  
+      
+    if not subdirs:  
+        raise FileNotFoundError("No checkpoint directories found in the specified directory.")  
+      
+    # Sort the directory names  
+    subdirs.sort()  
+    print("content of checkpoint_dir ", subdirs)
+    print("content inside latest checkpoint ", os.listdir(os.path.join(checkpoint_dir, subdirs[-1])))
+    # list the content of the directries under subdirs[-1]
+    sub_subdirs = [d for d in os.listdir(os.path.join(checkpoint_dir, subdirs[-1])) if os.path.isdir(os.path.join(checkpoint_dir, subdirs[-1], d))]
+    for sub_subdir in sub_subdirs:
+        print("sub sub inside latest checkpoint ", os.listdir(os.path.join(checkpoint_dir, subdirs[-1], sub_subdir)))
+   
+  
+    # Return the last directory in the sorted list, which should be the latest  
+    latest_tag = subdirs[-1]  
+    return latest_tag  
+  
+class MlflowLoggingCallback(TrainerCallback):  
+    def on_log(self, args, state, control, logs=None, **kwargs):  
+        if logs is not None:  
+            mlflow.log_metrics(logs, step=state.global_step)  
+            mlflow.log_metric('epoch', state.epoch)  
+  
+class EarlyStoppingCallback(TrainerCallback):  
+    def __init__(self, early_stopping_patience=1, early_stopping_threshold=0.0):  
+        self.early_stopping_patience = early_stopping_patience  
+        self.early_stopping_threshold = early_stopping_threshold  
+        self.best_metric = None  
+        self.patience_counter = 0  
+  
+    def on_evaluate(self, args, state, control, metrics=None, **kwargs):  
+        if metrics is not None and "eval_loss" in metrics:  
+            current_metric = metrics["eval_loss"]  
+            if self.best_metric is None or current_metric < self.best_metric - self.early_stopping_threshold:  
+                self.best_metric = current_metric  
+                self.patience_counter = 0  
+                control.should_save = True  
+                print(f"New best model found with eval_loss: {current_metric}")  
+            else:  
+                self.patience_counter += 1  
+                print(f"No improvement in eval_loss for {self.patience_counter} evaluations.")  
+                if self.patience_counter >= self.early_stopping_patience:  
+                    control.should_training_stop = True  
+                    print("Early stopping triggered.")  
+  
+def parse_args():  
+    parser = argparse.ArgumentParser()  
+    parser.add_argument("--model_dir", type=str)  
+    parser.add_argument("--epochs", type=int, default=1)  
+    parser.add_argument("--max_steps", type=int, default=1000)  
+    parser.add_argument("--trained_model", type=str, default="trained_model")  
+    parser.add_argument("--ckp_dir", type=str, default="ckp_dir")  
+    parser.add_argument("--train_dataset", type=str)  
+    parser.add_argument("--val_dataset", type=str)  
+    parser.add_argument("--train_batch_size", type=int, default=5)  
+    parser.add_argument("--val_batch_size", type=int, default=5)  
+    parser.add_argument("--lora_r", type=int, default=256)  
+    parser.add_argument("--lora_alpha", type=int, default=16)  
+    parser.add_argument("--lora_dropout", type=float, default=0.1)  
+    parser.add_argument("--bnb_4bit_compute_dtype", type=str, default="float16")  
+    parser.add_argument("--bnb_4bit_quant_type", type=str, default="nf4")  
+    parser.add_argument("--gradient_accumulation_steps", type=int, default=1)  
+    parser.add_argument("--max_seq_length", type=int, default=1280)  
+    parser.add_argument("--max_grad_norm", type=float, default=0.3)  
+    parser.add_argument("--learning_rate", type=float, default=2e-4)  
+    parser.add_argument("--weight_decay", type=float, default=0.001)  
+    parser.add_argument("--early_stopping_patience", type=int, default=3)  
+    parser.add_argument("--early_stopping_threshold", type=float, default=0.0)  
+    parser.add_argument("--chat_model", type=str2bool, nargs='?', const=True, default=False)  
+    parser.add_argument("--deepspeed", type=str2bool, nargs='?', const=True, default=False)  
+    parser.add_argument("--use_4bit", type=str2bool, nargs='?', const=True, default=True)  
+    parser.add_argument("--use_nested_quant", type=str2bool, nargs='?', const=True, default=False)  
+    parser.add_argument("--fp16", type=str2bool, nargs='?', const=True, default=False)  
+    parser.add_argument("--bf16", type=str2bool, nargs='?', const=True, default=True)  
+    parser.add_argument("--gradient_checkpointing", type=str2bool, nargs='?', const=True, default=True)  
+    parser.add_argument("--packing", type=str2bool, nargs='?', const=True, default=False)  
+  
+    return parser.parse_args()  
+  
+def main(args):  
+    PATH = args.model_dir  
+    trained_model = args.trained_model  
+    ckp_dir = args.ckp_dir
+    chat_model = args.chat_model  
+    deepspeed = args.deepspeed  
+    train_dataset = args.train_dataset  
+    val_dataset = args.val_dataset  
+    rank = int(os.environ.get('RANK', 0))  
+  
+    print("rank ", rank)  
+  
+    new_model = "fine_tuned_model"  
+    lora_r = args.lora_r  
+    lora_alpha = args.lora_alpha  
+    lora_dropout = args.lora_dropout  
+    use_4bit = args.use_4bit  
+    bnb_4bit_compute_dtype = args.bnb_4bit_compute_dtype  
+    bnb_4bit_quant_type = args.bnb_4bit_quant_type  
+    use_nested_quant = args.use_nested_quant  
+    fp16 = args.fp16  
+    bf16 = args.bf16  
+    per_device_train_batch_size = args.train_batch_size  
+    per_device_eval_batch_size = args.val_batch_size  
+    gradient_accumulation_steps = args.gradient_accumulation_steps  
+    gradient_checkpointing = args.gradient_checkpointing  
+    num_train_epochs = args.epochs  
+  
+    output_dir = ckp_dir  
+    max_grad_norm = args.max_grad_norm  
+    learning_rate = args.learning_rate  
+    weight_decay = args.weight_decay  
+    optim = "paged_adamw_32bit"  
+    lr_scheduler_type = "constant"  
+    max_steps = args.max_steps  
+    warmup_ratio = 0.03  
+    group_by_length = True  
+    save_steps=8  # Adjust based on your needs  
+    logging_steps = 5  
+    max_seq_length = args.max_seq_length  
+    packing = args.packing  
+  
+    local_rank = int(os.environ.get('LOCAL_RANK', '0'))  
+    device_map = {'': local_rank}  
+    # print("device map ", device_map)  
+  
+    train_data_dict = pd.read_json(train_dataset, lines=True).to_dict(orient="records")  
+    train_records = [item["record"] for item in train_data_dict]  
+    train_ds = Dataset.from_dict({"record": train_records})  
+  
+    val_data_dict = pd.read_json(val_dataset, lines=True).to_dict(orient="records")  
+    val_records = [item["record"] for item in val_data_dict]  
+    val_ds = Dataset.from_dict({"record": val_records})  
+  
+    def formatting_prompts_func(example):  
+        return example['record']  
+  
+    compute_dtype = getattr(torch, bnb_4bit_compute_dtype)  
+    bnb_config = BitsAndBytesConfig(  
+        load_in_4bit=use_4bit,  
+        bnb_4bit_quant_type=bnb_4bit_quant_type,  
+        bnb_4bit_compute_dtype=compute_dtype,  
+        bnb_4bit_use_double_quant=use_nested_quant,  
+    )  
+  
+    model = AutoModelForCausalLM.from_pretrained(  
+        os.path.join(PATH, "data", "model"),  
+        low_cpu_mem_usage=True,  
+        local_files_only=True,  
+        quantization_config=bnb_config,  
+        device_map=device_map  
+    )  
+  
+    model.config.use_cache = False  
+    model.config.pretraining_tp = 1  
+  
+    tokenizer = AutoTokenizer.from_pretrained(  
+        os.path.join(PATH, "data", "model"),  
+        local_files_only=True,  
+        device_map=device_map  
+    )  
+    tokenizer.pad_token = tokenizer.eos_token  
+    tokenizer.padding_side = "right"  
+  
+    peft_config = LoraConfig(  
+        lora_alpha=lora_alpha,  
+        lora_dropout=lora_dropout,  
+        r=lora_r,  
+        bias="none",  
+        task_type="CAUSAL_LM",  
+        target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],  
+    )  
+  
+    training_arguments = TrainingArguments(  
+        output_dir=output_dir,  
+        num_train_epochs=num_train_epochs,  
+        per_device_train_batch_size=per_device_train_batch_size,  
+        per_device_eval_batch_size=per_device_eval_batch_size,  
+        gradient_accumulation_steps=gradient_accumulation_steps,  
+        optim=optim,  
+        save_steps=save_steps,  
+        logging_steps=logging_steps,  
+        learning_rate=learning_rate,  
+        weight_decay=weight_decay,  
+        fp16=fp16,  
+        bf16=bf16,  
+        max_grad_norm=max_grad_norm,  
+        max_steps=max_steps,  
+        warmup_ratio=warmup_ratio,  
+        group_by_length=group_by_length,  
+        lr_scheduler_type=lr_scheduler_type,  
+        deepspeed="ds_config.json" if deepspeed else None,  
+    )  
+    training_arguments.ddp_find_unused_parameters = False  
+    response_template = "### Output:"  
+    data_collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)  
+  
+    early_stopping_callback = EarlyStoppingCallback(  
+        early_stopping_patience=args.early_stopping_patience,  
+        early_stopping_threshold=args.early_stopping_threshold  
+    )  
+  
+    trainer = SFTTrainer(  
+        model=model,  
+        train_dataset=train_ds,  
+        eval_dataset=val_ds,  
+        peft_config=peft_config,  
+        callbacks=[MlflowLoggingCallback()],  
+        max_seq_length=max_seq_length,  
+        tokenizer=tokenizer,  
+        args=training_arguments,  
+        packing=packing,  
+        formatting_func=formatting_prompts_func,  
+    )  
+    trainer.remove_callback(MLflowCallback)  
+  
+    with mlflow.start_run() as run:  
+        trainer.train() 
+        print("done with training") 
+  
+        if rank == 0:  
+            # Clear memory  
+            del model  
+            del trainer  
+            import gc  
+            gc.collect()  
+            ckp_tag = get_latest_checkpoint_tag(ckp_dir)
+            ckp_dir = os.path.join(ckp_dir, ckp_tag)
+            fp32_output_path = os.path.join(ckp_dir, "pytorch_model.bin")            
+            subprocess.run(
+                ['python', 'zero_to_fp32.py', '.', ckp_dir],
+                cwd=ckp_dir,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+                check=True
+            )
+            #copy from the converted_fp32 to the trained_model
+            dest_path = os.path.join(trained_model, 'lora')
+            os.makedirs(dest_path, exist_ok=True)
+            shutil.copy(fp32_output_path, dest_path)       
+            # Copy config.json and tokenizer files to dest_path
+            model_files = ['adapter_config.json','adapter_model.safetensors','tokenizer.json', 'tokenizer_config.json', 'special_tokens_map.json']
+
+            for file_name in model_files:
+                src_file = os.path.join(ckp_dir, file_name)
+                if os.path.exists(src_file):
+                    shutil.copy(src_file, dest_path)
+                else:
+                    print(f"Warning: {file_name} not found in {ckp_dir}")
+
+                
+
+        else:  
+            print("at rank ", rank)  
+  
+if __name__ == "__main__":  
+    args = parse_args()  
+    main(args)  
