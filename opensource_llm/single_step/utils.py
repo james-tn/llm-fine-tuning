772ccd00465529996c1d11a5f470f9da8c52d3b3
james-tn <james.nguyen@microsoft.com>
2025-01-21 12:01:49 -0800

    add different run structure

--- opensource_llm/single_step/utils.py
+++ opensource_llm/single_step/utils.py
@@ -130,7 +130,7 @@ def create_and_prepare_model(args, data_args, training_args):
         )
     else:
         torch_dtype = (
-            quant_storage_dtype if quant_storage_dtype and quant_storage_dtype.is_floating_point else torch.float32
+            quant_storage_dtype if quant_storage_dtype and quant_storage_dtype.is_floating_point else torch.bfloat16
         )
         model = AutoModelForCausalLM.from_pretrained(
             args.model_name_or_path,
